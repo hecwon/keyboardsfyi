b3a71c0f249c06e4b19180777097fa68877dd15a
hecwon <hecwon@gmail.com>
2023-05-05 15:55:21 -0700

    add image placeholder on main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,28 @@ import Link from 'next/link';
 import Layout from '../../components/global/layout';
 import styles from './page.module.css'
 import './globals.css';
+import Image from 'next/image';
 
 const Home = () => {
   return (
     <Layout>
       <main className={styles.main}>
           <div>
+            <div className={styles.block}></div>
+            <div className={styles.block}></div>
+            <Image
+              src="/keyboard.png"
+              height={400}
+              width={600}
+              alt="Keyboard Clipart"
+            />
+            <div className={styles.block}></div>
             <p>THIS IS A WIP</p>
             <p> Hello, Welcome to KEYBOARDS.FYI</p>
             <p>Home of the first User-Generated Keyboard Enthusiast Database!</p>
             <Link href="/main" className={styles.card}>
               <h2>
-                Find It <span>-&gt;</span>
+                SEARCH <span>-&gt;</span>
               </h2>
             </Link>
           </div>
